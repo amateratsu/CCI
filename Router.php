76868b3c0adb2b4f577a26724b8b173af0ce79d3
Unknown <lement.aymeric@outlook.com>
2017-12-12 11:43:40 +0100

    hvfd
    kjh

--- Router.php
+++ Router.php
@@ -21,6 +21,78 @@ class Router
             case 'list':
                 $offersController->listOffers();
                 break;
+            case '404':
+                include '404.php';
+                break;
+            case 'contact':
+                $contactController->contact();
+                break;
+            case 'faq':
+                include 'faq.php';
+                break;
+            case 'legal':
+                include 'legal.php';
+                break;
+            case 'blog':
+                $blogController->blog();
+                break;
+            case 'reviews':
+                $reviewsController->reviews();
+                break;
+            case 'offres_bureaux':
+                $offersController->listOffers();
+                break;
+            case 'offres_commerces':
+                $offersController->listOffers();
+                break;
+            case 'offres_entrepots':
+                $offersController->listOffers();
+                break;
+            case 'offres_industries':
+                $offersController->listOffers();
+                break;
+            case 'offres_salles':
+                $offersController->listOffers();
+                break;
+            case 'offres_terrains':
+                $offersController->listOffers();
+                break;
+            case 'logcollec':
+                $logController->log();
+                break;
+            case 'logpart':
+                $logController->log();
+                break;
+            case 'logentre':
+                $logController->log();
+                break;
+            case 'listentre':
+                $affiliatesController->listAffil();
+                break;
+            case 'register':
+                $logController->log();
+                break;
+            case 'profil':
+                $logController->log();
+                break;
+            case 'detail':
+                $offersController->offer();
+                break;
+            case 'edit_listing':
+                $offersController->offer();
+                break;
+            case 'connexion':
+                $logController->log();
+                break;
+            case 'listcollec':
+                $affiliatesController->listAffil();
+                break;
+            case 'listpart':
+                $affiliatesController->listAffil();
+                break;
+            case 'reset_password':
+                $affiliatesController->affilInfo();
+                break;
             case 'offer':
                 // cette méthode a un paramètre "id" obligatoire
                 if (!isset($_GET['id'])) {
