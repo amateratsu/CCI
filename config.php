9d4d1c17c6a4a4624fc4c3e3153cd201f3408f40
Unknown <amaterasu.sempai@gmail.com>
2017-12-12 15:44:53 +0100

    réparé

--- config.php
+++ config.php
@@ -7,13 +7,10 @@ ini_set('display_errors',1);
 header('Content-Type: text/html; charset=utf-8');
 define('hostServer','127.0.0.1');
 define('DbUser','root');
-<<<<<<< HEAD
-define('DbPassword','simplon');
-define('Database','immocci');
-=======
-define('DbPassword','milka');
-define('Database','immo bourse');
->>>>>>> master
+
+define('DbPassword','A8q8w8z8s8x8--');
+define('Database','CCI');
+
 
 // Connexion mysqli
 $mysqli = mysqli_connect(hostServer, DbUser, DbPassword, Database);
